4025e217a6b118dd9cfc1aed0aad0af6fbafcedc
unknown <valentinechima95@gmail.com>
2023-01-25 23:08:18 +0100

    update on pages

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-let btn = document.querySelector('.about');
-let for_about = document.querySelector('.for-about');
-let bot = document.querySelector('.bot')
+// let btn = document.querySelector('.about');
+// let for_about = document.querySelector('.for-about');
+// let bot = document.querySelector('.bot')
 
-for_about.style.display = 'none'
-bot.addEventListener('click', () => {
-    for_about.style.display = 'none'
-})
+// for_about.style.display = 'none'
+// bot.addEventListener('click', () => {
+//     for_about.style.display = 'none'
+// })
 
-btn.addEventListener('click', () =>{
-    if(for_about.style.display === 'none'){
-        for_about.style.display = 'block';
-        setTimeout(() => for_about.classList.remove(".for-about"), 10000)
-    }
-    else {
-        for_about.style.display = 'none';
-    }
-})
+// btn.addEventListener('click', () =>{
+//     if(for_about.style.display === 'none'){
+//         for_about.style.display = 'block';
+//         setTimeout(() => for_about.classList.remove(".for-about"), 10000)
+//     }
+//     else {
+//         for_about.style.display = 'none';
+//     }
+// })
 
 
 
@@ -43,4 +43,5 @@ btn.addEventListener('click', () =>{
 //     duration: 1500
 // });
 
-// ScrollReveal().reveal('.menu-inner1 h4', { delay: 50, interval:100, origin: 'bottom' });
+// ScrollReveal().reveal('.in-in-about h1, .in-in-about2 h3', { delay: 50, interval:100, origin: 'top' });
+// ScrollReveal().reveal('.in-in-about h4, .in-in-about2 h4, .testimonies', { delay: 50, interval:100, origin: 'bottom' });
