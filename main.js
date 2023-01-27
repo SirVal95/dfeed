1e079471a9b9a6f81cceafd03d3e7a79494e97b9
unknown <valentinechima95@gmail.com>
2023-01-27 09:38:11 +0100

    page transition

--- main.js
+++ main.js
@@ -18,7 +18,31 @@
 // })
 
 
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('a')
 
+    setTimeout(() => {
+        transition_el.classList.remove('is-active')
+    }), 1000;
+
+    // for (let i = 0; i < anchors.length; i++) {
+    //     const anchor = anchors[i];
+
+    //     anchor.addEventListener('click', e => {
+    //         e.preventDefault();
+    //         let target = e.target.href;
+
+    //         transition_el.classList.add('is-active');
+
+    //         setTimeout(() => {
+    //             window.location.href = target;
+    //         }, 1000);
+    //     })
+
+        
+    // }
+}
 
 
 
