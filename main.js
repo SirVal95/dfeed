dd427842d989bf42be2fa8149b6e30e5bfb3a564
unknown <valentinechima95@gmail.com>
2023-01-28 21:21:49 +0100

    update on scroll up

--- main.js
+++ main.js
@@ -26,6 +26,10 @@ window.onload = () => {
         transition_el.classList.remove('is-active')
     }), 1000;
 
+
+
+
+// document.body.scrollTop > 20 ||
     // for (let i = 0; i < anchors.length; i++) {
     //     const anchor = anchors[i];
 
@@ -45,6 +49,26 @@ window.onload = () => {
 }
 
 
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
 
 
 
@@ -58,12 +82,13 @@ window.onload = () => {
 
 
 
-// ScrollReveal({
-//     reset: true,
-//     distance:'60px',
-//     delay: 50,
-//     duration: 1500
-// });
 
-// ScrollReveal().reveal('.in-in-about h1, .in-in-about2 h3', { delay: 50, interval:100, origin: 'top' });
-// ScrollReveal().reveal('.in-in-about h4, .in-in-about2 h4, .testimonies', { delay: 50, interval:100, origin: 'bottom' });
+ScrollReveal({
+    reset: true,
+    distance:'60px',
+    delay: 50,
+    duration: 1500
+});
+
+ScrollReveal().reveal('.for-about h1, .for-about h3, .for-about h4, .in-in-about2 .p1', { delay: 50, interval:100, origin: 'top' });
+ScrollReveal().reveal('.testimonies, .in-in-about2 .img1, .footer', { delay: 50, interval:100, origin: 'bottom' });
